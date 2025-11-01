50fab0be774223e9890af860bc7a0deff90683c9
trshangyn <trshangyn@gmail.com>
2025-10-31 21:22:34 -0500

    merging trish-4

--- backend/models/Notifs.js
+++ backend/models/Notifs.js
@@ -1,15 +1,27 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema; // Import Schema from mongoose
 
-const notifsSchema = new Schema({
-  title: { type: String },
-  event: { type: Schema.Types.ObjectId, ref: "Event", required: false }, // Reference to the Event Schema
-  eventName: { type: String },
-  eventDescription: { type: String }, // Add description
-  location: { type: String }, // Add location
-  eventDate: { type: Date }, // Add date
-  user: { type: Schema.Types.ObjectId, ref: "User", required: false },
-});
+const notifsSchema = new Schema(
+  {
+    title: { type: String },
+    message: { type: String }, // keep route's message
+    event: {
+      type: Schema.Types.ObjectId,
+      ref: "EventDetails",
+      required: false,
+    },
+    eventName: { type: String },
+    eventDescription: { type: String }, // Add description
+    location: { type: String }, // Add location
+    eventDate: { type: Date }, // Add date
+    user: {
+      type: Schema.Types.ObjectId,
+      ref: "UserCredentials",
+      required: false,
+    },
+  },
+  { timestamps: true }
+); // adding createdAt/updatedAt fields
 
 const Notifs = mongoose.model("Notifs", notifsSchema);
 
